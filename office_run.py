717c13421debdd7b0455e66169acb771f124ca2d
Guangrui Li <guangrui.li-1@student.uts.edu.au>
2021-07-30 15:15:35 +0800

    Update office_run.py

--- office_run.py
+++ office_run.py
@@ -34,7 +34,7 @@ def main():
         config.cls_total = 31
     elif config.setting=='osda':
         config.cls_share = 10
-        config.cls_src   = 0
+        config.cls_src   = 10
         config.cls_total = 31
     elif config.setting=='pda':
         config.cls_share = 10
@@ -45,9 +45,12 @@ def main():
     config.uk_index=config.cls_share + config.cls_src
     a,b,c =  config.cls_share, config.cls_src, config.cls_total
     c = c-a-b
+
     share_classes  = [i for i in range(a)]
     source_classes = [a+i for i in range(b)]
     target_classes = [a+b+i for i in range(c)]
+    if config.setting=='osda':
+        source_classes = []
     config.share_classes  = share_classes
     config.source_classes = share_classes + source_classes
     config.target_classes = share_classes + target_classes
